f4019d2d717d7397558663e4ce1c292d80c0969c
Bruna Campos <brunareginadecampos@gmail.com>
2025-04-28 13:00:14 -0300

    Código Finalizado

---
+++ forca.h
@@ -0,0 +1,13 @@
+#define TAMANHO_PALAVRA 20
+
+int letraexiste(char letra);
+int chuteserrados();
+int enforcou();
+int ganhou();
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+void adicionapalavra();
+int main();
